c83bb8273f3eea6ab6e3a5612d3937927934e7c9
MatheusSC <matheuscouto.tds2.senai>
2024-03-19 17:11:14 -0300

    ex2.1

--- vogais.js
+++ vogais.js
@@ -16,11 +16,16 @@ for(let i = 0; i < arrayLetras.length; i++){
     if(vogais.includes(arrayLetras[i])){
         arrayVogais.push(arrayLetras[i]);
     }
-    else(consoantes.includes(arrayLetras[i])){
-        arrayConsoantes.push(arrayLetras[i])
+}
+for(let i = 0; i < arrayLetras.length; i++){
+    if(consoantes.includes(arrayLetras[i])){
+        arrayConsoantes.push(arrayLetras[i]);
     }
+  
 }
 
 console.log("Texto original: ", texto);
 console.log("Vogais encontradas: ", arrayVogais.toString());
 console.log("Total de vogais: " + arrayVogais.length)
+console.log("Vogais encontradas: ", arrayConsoantes.toString());
+console.log("Total de consoantes: " + arrayConsoantes.length)
