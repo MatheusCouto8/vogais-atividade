8bdca5e39b16e1df772b946942d50cd1eb055379
Matheus Couto <158165513+MatheusCouto8@users.noreply.github.com>
2024-03-20 20:53:30 -0300

    ex2.pronto

--- vogais.js
+++ vogais.js
@@ -2,7 +2,7 @@ let vogais = ['a', 'e', 'i', 'o', 'u'];
 
 let consoantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v','w', 'x', 'y', 'z'];
 
-let texto = "Rei de Roma";
+let texto = "O rei de Roma";
 
 texto = texto.toLowerCase();
 
@@ -12,20 +12,23 @@ let arrayVogais = [];
 
 let arrayConsoantes = [];
 
+let arrayEspacos = [];
+
 for(let i = 0; i < arrayLetras.length; i++){
     if(vogais.includes(arrayLetras[i])){
         arrayVogais.push(arrayLetras[i]);
     }
-}
-for(let i = 0; i < arrayLetras.length; i++){
     if(consoantes.includes(arrayLetras[i])){
         arrayConsoantes.push(arrayLetras[i]);
     }
-  
+    if(arrayLetras[i] === ' ') {
+        arrayEspacos++;
+    }
 }
 
 console.log("Texto original: ", texto);
 console.log("Vogais encontradas: ", arrayVogais.toString());
-console.log("Total de vogais: " + arrayVogais.length)
+console.log("Total de vogais: " + arrayVogais.length);
 console.log("Vogais encontradas: ", arrayConsoantes.toString());
-console.log("Total de consoantes: " + arrayConsoantes.length)
+console.log("Total de consoantes: " + arrayConsoantes.length);
+console.log("Total de espaços: " + arrayEspacos);
